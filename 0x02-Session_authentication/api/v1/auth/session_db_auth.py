2caac4d18f7e386f5e8e7f919de42ec6959c6dfb
LabyKreative <labykreative@yahoo.com>
2023-11-09 10:14:55 +0000

    Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
-"""
-Define class SessionDButh
-"""
+"""Class SessionDButh"""
 from .session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
-    """
-    Definition of SessionDBAuth class that persists session data
-    in a database
-    """
+    """Define SessionDBAuth class that persists session database"""
 
     def create_session(self, user_id=None):
-        """
-        Create a Session ID for a user_id
-        Args:
-           user_id (str): user id
-        """
+        """Create a Session ID for a user_id"""
         session_id = super().create_session(user_id)
         if not session_id:
             return None
@@ -30,23 +21,14 @@ class SessionDBAuth(SessionExpAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """
-        Returns a user ID based on a session ID
-        Args:
-            session_id (str): session ID
-        Return:
-            user id or None if session_id is None or not a string
-        """
+        """Returns a user ID based on a session ID"""
         user_id = UserSession.search({"session_id": session_id})
         if user_id:
             return user_id
         return None
 
     def destroy_session(self, request=None):
-        """
-        Destroy a UserSession instance based on a
-        Session ID from a request cookie
-        """
+        """Destroys a UserSession instance based on a Session ID"""
         if request is None:
             return False
         session_id = self.session_cookie(request)
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-""" UserSession module
-"""
+"""UserSession module"""
 from models.base import Base
 
 
 class UserSession(Base):
-    """
-    UserSession class
-    """
+    """UserSession class"""
 
     def __init__(self, *args: list, **kwargs: dict):
-        """
-        Initialize a UserSession instance
-        """
+        """Initialize a UserSession instance"""
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
